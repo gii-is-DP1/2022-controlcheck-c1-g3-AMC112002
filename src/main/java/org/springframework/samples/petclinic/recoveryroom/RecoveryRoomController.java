7c11f3830c994c2ce95857b9b33e9c7b0a9882b6
AMC112002 <alejandromateocapilla@gmail.com>
2022-11-03 17:28:23 +0100

    Entrega examen (control check)

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomController.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomController.java
@@ -1,5 +1,46 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/recoveryroom")
 public class RecoveryRoomController {
+	
+	 private static final String VIEW_RECOVERYROOMS_CREATE_FORM = "recoveryroom/createOrUpdateRecoveryRoomForm";
+	    private static final String WELCOME = "welcome";
+	    private RecoveryRoomService rrservice;
+	    
+	    @Autowired
+	    public RecoveryRoomController(RecoveryRoomService service) {
+	    	this.rrservice = service;
+	    }
+	    
+	    @GetMapping("/create") 	
+	    public String initRecoveryRoom(ModelMap map) {
+	    	map.addAttribute("recoveryroom", new RecoveryRoom());
+	    	map.addAttribute("types", rrservice.getAllRecoveryRoomTypes());
+	    	return VIEW_RECOVERYROOMS_CREATE_FORM;
+	    }
+	    
+	    @PostMapping(path = "/create")
+	    public String createRecoveryRoom(@Valid RecoveryRoom recoveryroom, BindingResult br, ModelMap map){
+	        if(!br.hasErrors()){
+	        	rrservice.save(recoveryroom);
+	            map.addAttribute("message", "Recovery Room succesfully save");
+	            return WELCOME;
+	        } else {
+	            map.addAttribute("recovery room", recoveryroom);
+	            map.addAttribute("types", rrservice.getAllRecoveryRoomTypes());
+	        }
+	        return VIEW_RECOVERYROOMS_CREATE_FORM;
+	    }
     
 }
--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
@@ -1,12 +1,21 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
 import java.util.List;
+
 import java.util.Optional;
 
-public interface RecoveryRoomRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecoveryRoomRepository extends CrudRepository<RecoveryRoom,Integer> {
     List<RecoveryRoom> findAll();
     Optional<RecoveryRoom> findById(int id);
     RecoveryRoom save(RecoveryRoom p);
-    //List<RecoveryRoomType> findAllRecoveryRoomTypes();
-    //RecoveryRoomType getRecoveryRoomType(String name);
+    @Query("SELECT pt FROM RecoveryRoomType pt")
+    List<RecoveryRoomType> findAllRecoveryRoomTypes();
+    @Query("SELECT pt FROM RecoveryRoomType pt WHERE pt.name = :name")
+    RecoveryRoomType getRecoveryRoomType(@Param("name") String name);
 }
